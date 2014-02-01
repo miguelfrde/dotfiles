da2daa5a68cda353a8d9e94138f91557b08f1b61
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-01-31 18:25:32 -0600

    Add some zsh-syntax-highlighting, solarized

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ echo_title "Sublime configuration"
 source sublime/sublime-setup.sh
 echo_done "Sublime configuration"
 
-echo "Set up environment"
+echo_title "some configurations"
 
 # As stated in R caveats, do this to use brew R with RStudio
 sudo ln -s "/usr/local/opt/r/R.framework" /Library/Frameworks
@@ -73,19 +73,44 @@ sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
 sudo bash -c "echo /usr/local/bin/zsh >> /etc/shells"
 sudo bash -c "echo /usr/local/bin/zsh >> /private/etc/shells"
 
+echo_done "some configurations"
+
+echo_title "oh-my-zsh!"
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+echo_done "oh-my-zsh"
+
+echo_title "zsh stuff: miguelfrde theme and zsh-syntax-highlighting"
+mkdir ~/.oh-my-zsh/custom/themes
+mkdir ~/.oh-my-zsh/custom/plugins 
+mkdir ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+mv ~/.miguelfrde.zsh-theme ~/.oh-my-zsh/custom/themes/miguelfrde.zsh-theme
+cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+cd $SETUP_DIR
+echo_done "zsh stuff: miguelfrde theme and zsh-syntax-highlighting"
+
+echo_title "vim stuff"
+mkdir -p ~/.vim/colors
+curl -o solarized.vim https://raw2.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+mv solarized.vim ~/.vim/colors
+echo_done "vim stuff"
+
+echo_title "dotfiles"
 copy_dotfiles "common"
 copy_dotfiles "bash"
 copy_dotfiles "git"
 copy_dotfiles "zsh"
-mv ~/.miguelfrde.zsh-theme ~/.oh-my-zsh/custom/themes/miguelfrde.zsh-theme
+echo_done "dotfiles"
 
 #source ~/.bash_profile
 #source ~/.zshrc
-source osx.sh
 
-echo_title "oh-my-zsh!"
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-echo_done "oh-my-zsh!"
+echo_title "OSX configurations"
+source osx.sh
+echo_done "OSX configurations"
+
+echo "Changing shell to zsh..."
+chsh -s `which zsh`
 
 echo "Setup finished. You may need to restart the computer."
 read -p "Press [Enter] to exit this utility..."
