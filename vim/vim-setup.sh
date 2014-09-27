40e0ca7a39b2a7f854eef64a1deb390c018270b8
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-09-27 18:56:23 +0200

    Update vimrc

--- vim/vim-setup.sh
+++ vim/vim-setup.sh
@@ -10,3 +10,10 @@ cp vimrc ~/.vimrc
 echo "Installing vundle and vim plugins..."
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+
+mkdir ycm_build
+cd ycm_build
+cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=~/ycm_temp/llvm_root_dir . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+make ycm_support_libs
+cd ..
+rm -rf ycm_build
