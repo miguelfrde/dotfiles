c38dfeb1d2607a6eee657c99a75c562de1e224ac
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-08-25 08:01:24 -0500

    Setup powerline fonts for vim-airline

--- vim/vim-setup.sh
+++ vim/vim-setup.sh
@@ -5,4 +5,8 @@ echo "Installing vundle and vim plugins..."
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
 
-
+echo "Install powerline fonts for vim-airline..."
+git clone https://github.com/powerline/fonts
+./fonts/install.sh
+rm -rf fonts
+echo "NOTE: set iTerm > Profile > Text > Non ASCII font to be a powerline one"
