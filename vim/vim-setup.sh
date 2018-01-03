d7b95e728efd96ceee81b9a553bd521fb5a35fb1
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2018-01-03 11:13:10 -0800

    Update vim-setup.sh

--- vim/vim-setup.sh
+++ vim/vim-setup.sh
@@ -2,7 +2,7 @@ echo "Linking .vimrc..."
 ln -sf `pwd`/vim/vimrc ~/.vimrc
 
 echo "Installing vundle and vim plugins..."
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
 
 echo "Install powerline fonts for vim-airline..."
